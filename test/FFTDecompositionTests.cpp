d3bd927f4297ec498b62d8c00a116d205f093389
LeonPS <leon@leonps.com>
2024-01-18 16:40:07 +0000

    test completes decomposed FFT

--- test/FFTDecompositionTests.cpp
+++ test/FFTDecompositionTests.cpp
@@ -1,86 +1,112 @@
 #include <catch2/catch_test_macros.hpp>
+#include <juce_dsp/juce_dsp.h>
 #include <zones_convolver/util/ComplexBuffer.h>
 #include <zones_convolver/util/FFTDecomposition.h>
 
 static bool ApproximatelyEqualComplex (const std::complex<float> & a, const std::complex<float> & b)
 {
-    return juce::approximatelyEqual (a.real (), b.real ()) &&
-           juce::approximatelyEqual (a.imag (), b.imag ());
+    auto tolerance = juce::Tolerance<float> ().withAbsolute (0.1f);
+    return juce::approximatelyEqual (a.real (), b.real (), tolerance) &&
+           juce::approximatelyEqual (a.imag (), b.imag (), tolerance);
+}
+
+inline void BitReverseSwap (std::complex<float> * input, unsigned n)
+{
+    for (unsigned i = 0, j = 0; i < n; i++)
+    {
+        if (i < j)
+        {
+            auto tmp = input [i];
+            input [i] = input [j];
+            input [j] = tmp;
+        }
+
+        unsigned mask = i ^ (i + 1);
+        unsigned rev = n / (mask + 1);
+        j ^= n - rev;
+    }
 }
 
 TEST_CASE ("changing step size does not affect decomposition output",
            "[ForwardDecompositionRadix2]")
 {
-    static constexpr auto kSubDecompositionSize = 32u;
-    auto num_step_sizes = static_cast<std::size_t> (std::log2 (kSubDecompositionSize));
-
-    ComplexBuffer inputs {kSubDecompositionSize, num_step_sizes};
-
-    for (auto channel_index = 0; channel_index < inputs.GetNumChannels (); ++channel_index)
+    for (auto num_points : {8u, 16u, 32u, 64u, 128u})
     {
-        auto channel_pointer = inputs.GetWritePointer (channel_index);
-        for (auto point_index = 0u; point_index < kSubDecompositionSize; ++point_index)
+        auto num_step_sizes = static_cast<std::size_t> (std::log2 (num_points));
+        ComplexBuffer inputs {num_points, num_step_sizes};
+
+        for (auto channel_index = 0u; channel_index < inputs.GetNumChannels (); ++channel_index)
         {
-            channel_pointer [point_index] = {static_cast<float> (point_index) + 1.f, 0.f};
+            auto channel_pointer = inputs.GetWritePointer (channel_index);
+            for (auto point_index = 0u; point_index < num_points; ++point_index)
+            {
+                channel_pointer [point_index] = {static_cast<float> (point_index) + 1.f, 0.f};
+            }
         }
-    }
 
-    for (auto step_size_index = 0u; step_size_index < num_step_sizes; ++step_size_index)
-    {
-        auto step_size = static_cast<std::size_t> (std::pow (2, step_size_index));
-        auto num_steps = kSubDecompositionSize / (2 * step_size);
-        for (auto step = 0u; step < num_steps; ++step)
-            FFTDecomposition::ForwardDecompositionRadix2 (
-                inputs.GetWritePointer (step_size_index), kSubDecompositionSize, num_steps, step);
-    }
+        for (auto step_size_index = 0u; step_size_index < num_step_sizes; ++step_size_index)
+        {
+            auto step_size = static_cast<std::size_t> (std::pow (2, step_size_index));
+            auto num_steps = num_points / (2 * step_size);
+            for (auto step = 0u; step < num_steps; ++step)
+                FFTDecomposition::ForwardDecompositionRadix2 (
+                    inputs.GetWritePointer (step_size_index), num_points, num_steps, step);
+        }
 
-    for (auto step_size_index = 0u; step_size_index < num_step_sizes - 1; ++step_size_index)
-        for (auto point_index = 0u; point_index < kSubDecompositionSize; ++point_index)
-            REQUIRE (ApproximatelyEqualComplex (
-                inputs.GetReadPointer (step_size_index) [point_index],
-                inputs.GetReadPointer (step_size_index + 1u) [point_index]));
+        for (auto step_size_index = 0u; step_size_index < num_step_sizes - 1; ++step_size_index)
+            for (auto point_index = 0u; point_index < num_points; ++point_index)
+                REQUIRE (ApproximatelyEqualComplex (
+                    inputs.GetReadPointer (step_size_index) [point_index],
+                    inputs.GetReadPointer (step_size_index + 1u) [point_index]));
+    }
 }
 
-//    GIVEN ("a complex array")
-//    {
-//        std::array<std::complex<float>, 8> input {
-//            std::complex<float> {1.f, 0.f},
-//            std::complex<float> {2.f, 0.f},
-//            std::complex<float> {3.f, 0.f},
-//            std::complex<float> {4.f, 0.f},
-//            std::complex<float> {5.f, 0.f},
-//            std::complex<float> {6.f, 0.f},
-//            std::complex<float> {7.f, 0.f},
-//            std::complex<float> {8.f, 0.f},
-//        };
-//
-//        auto input_size = input.size ();
-//        auto num_decomposition_layers = static_cast<std::size_t> (std::log2 (input_size));
-//        auto step_size = 2u;
-//
-//        for (auto decomposition_layer = 0u; decomposition_layer <
-//        num_decomposition_layers;
-//             ++decomposition_layer)
-//        {
-//            auto num_sub_decompositions =
-//                static_cast<std::size_t> (std::pow (2, decomposition_layer));
-//            auto sub_decomposition_size = input_size / num_sub_decompositions;
-//
-//            for (auto sub_decomposition = 0u; sub_decomposition < num_sub_decompositions;
-//                 ++sub_decomposition)
-//            {
-//                auto num_steps = sub_decomposition_size / step_size;
-//                for (auto step = 0u; step < num_steps /2; ++step)
-//                {
-//                    FFTDecomposition::ForwardDecompositionRadix2 (
-//                        &input [(sub_decomposition * sub_decomposition_size)],
-//                        sub_decomposition_size,
-//                        num_steps,
-//                        step);
-//                }
-//            }
-//        }
-//
-//        for (auto point : input)
-//            std::cout << point << "\n";
-//    }
+TEST_CASE ("completes decomposed FFT", "[ForwardDecompositionRadix2]")
+{
+    for (auto num_points : {2u, 4u, 8u, 32u, 64u, 1024u})
+    {
+        static constexpr auto kDecomposedFFTIndex = 0;
+
+        ComplexBuffer input {num_points, 2};
+        input.Clear ();
+
+        auto decomposed_fft_channel = input.GetWritePointer (0);
+        auto juce_fft_channel = input.GetWritePointer (1);
+
+        for (auto point_index = 0u; point_index < num_points; ++point_index)
+            decomposed_fft_channel [point_index] = {static_cast<float> (point_index) + 1.f, 0.f};
+
+        auto fft_order = static_cast<int> (std::log2 (num_points));
+        auto juce_fft = juce::dsp::FFT {fft_order};
+        juce_fft.perform (decomposed_fft_channel, juce_fft_channel, false);
+
+        auto num_decomposition_layers = static_cast<std::size_t> (std::log2 (num_points));
+        for (auto decomposition_layer = 0u; decomposition_layer < num_decomposition_layers;
+             ++decomposition_layer)
+        {
+            auto num_sub_decompositions =
+                static_cast<std::size_t> (std::pow (2, decomposition_layer));
+            auto sub_decomposition_num_points = num_points / num_sub_decompositions;
+            for (auto sub_decomposition = 0u; sub_decomposition < num_sub_decompositions;
+                 ++sub_decomposition)
+            {
+                FFTDecomposition::ForwardDecompositionRadix2 (
+                    &decomposed_fft_channel [sub_decomposition * sub_decomposition_num_points],
+                    sub_decomposition_num_points,
+                    1,
+                    0);
+            }
+        }
+
+        /**
+         * JUCE FFT performs DIT, to ensure points within each output have the same order a bit
+         * reverse swap is performed on the DIF FFT outputs.
+         */
+        BitReverseSwap (decomposed_fft_channel, num_points);
+
+        for (auto point_index = 0u; point_index < num_points; ++point_index)
+
+            REQUIRE (ApproximatelyEqualComplex (decomposed_fft_channel [point_index],
+                                                juce_fft_channel [point_index]));
+    }
+}
