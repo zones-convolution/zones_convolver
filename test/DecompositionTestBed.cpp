63e3fd716f79a966a8abe7534c723982801cc143
Micah Strange <micasans@btinternet.com>
2024-02-13 12:38:26 +0000

    modify decomposition scheme

--- test/DecompositionTestBed.cpp
+++ test/DecompositionTestBed.cpp
@@ -29,8 +29,14 @@ void FillStageBuffersHalfWay (StageBuffers & stage_buffers, int input_size)
         stage_a [0 + sample_index] = std::complex<float> {0.5f, 0.f};
 }
 
+// STAGE,
+// PARTITION SIZE,
+// DIVISIONS,
+// NUM PHASES,
+// PHASE
 void TestBedForwardDecompositionRadix2 (std::complex<float> * data,
                                         std::size_t num_points,
+                                        std::size_t num_segments,
                                         std::size_t num_steps,
                                         std::size_t current_step)
 {
@@ -38,27 +44,32 @@ void TestBedForwardDecompositionRadix2 (std::complex<float> * data,
     jassert (num_steps > 0 && num_steps <= num_points && juce::isPowerOfTwo (num_steps));
     jassert (current_step < num_steps);
 
-    auto half_num_points = num_points / 2;
+    auto segment_num_points = num_points / num_segments;
+    auto half_num_points = segment_num_points / 2;
     auto step_size = half_num_points / num_steps;
     auto start_index = current_step * step_size;
     auto w_n = std::exp (std::complex<float> (
-        0.0f, -juce::MathConstants<float>::twoPi / static_cast<float> (num_points)));
+        0.0f, -juce::MathConstants<float>::twoPi / static_cast<float> (segment_num_points)));
 
-    for (auto point_index = start_index; point_index < start_index + step_size; ++point_index)
+    for (auto segment_index = 0; segment_index < num_segments; ++segment_index)
     {
-        jassert (half_num_points + point_index < num_points);
+        auto segment = &data [segment_index * segment_num_points];
+        for (auto point_index = start_index; point_index < start_index + step_size; ++point_index)
+        {
+            jassert (half_num_points + point_index < segment_num_points);
+            auto point = segment [point_index];
 
-        auto point = data [point_index];
-
-        data [point_index] += data [half_num_points + point_index];
-        data [half_num_points + point_index] =
-            (point - data [half_num_points + point_index]) *
-            std::pow (w_n, std::complex<float> {static_cast<float> (point_index), 0.0f});
+            segment [point_index] += segment [half_num_points + point_index];
+            segment [half_num_points + point_index] =
+                (point - segment [half_num_points + point_index]) *
+                std::pow (w_n, std::complex<float> {static_cast<float> (point_index), 0.0f});
+        }
     }
 }
 
 void TestBedInverseDecompositionRadix2 (std::complex<float> * data,
                                         std::size_t num_points,
+                                        std::size_t num_segments,
                                         std::size_t num_steps,
                                         std::size_t current_step)
 {
@@ -66,20 +77,25 @@ void TestBedInverseDecompositionRadix2 (std::complex<float> * data,
     jassert (num_steps > 0 && num_steps <= num_points && juce::isPowerOfTwo (num_steps));
     jassert (current_step < num_steps);
 
-    auto half_num_points = num_points / 2;
+    auto segment_num_points = num_points / num_segments;
+    auto half_num_points = segment_num_points / 2;
     auto step_size = half_num_points / num_steps;
     auto start_index = current_step * step_size;
     auto w_n = std::exp (std::complex<float> (
-        0.0f, juce::MathConstants<float>::twoPi / static_cast<float> (num_points)));
+        0.0f, juce::MathConstants<float>::twoPi / static_cast<float> (segment_num_points)));
 
-    for (auto point_index = start_index; point_index < start_index + step_size; ++point_index)
+    for (auto segment_index = 0; segment_index < num_segments; ++segment_index)
     {
-        jassert (half_num_points + point_index < num_points);
-        auto b_n = data [point_index + half_num_points] *
-                   std::pow (w_n, std::complex<float> {static_cast<float> (point_index), 0.0f});
+        auto segment = &data [segment_index * segment_num_points];
+        for (auto point_index = start_index; point_index < start_index + step_size; ++point_index)
+        {
+            jassert (half_num_points + point_index < segment_num_points);
+            auto b_n = segment [point_index + half_num_points] *
+                       std::pow (w_n, std::complex<float> {static_cast<float> (point_index), 0.0f});
 
-        data [half_num_points + point_index] = (data [point_index] - b_n) * 0.5f;
-        data [point_index] = (data [point_index] + b_n) * 0.5f;
+            segment [half_num_points + point_index] = (segment [point_index] - b_n) * 0.5f;
+            segment [point_index] = (segment [point_index] + b_n) * 0.5f;
+        }
     }
 }
 
@@ -96,30 +112,20 @@ static void HandleStageA (int kBlockSize,
             std::complex<float> {input_buffer.getSample (0, stage_index + sample_index), 0.f};
 
     // Level 0 decompositions happen in every phase
-    TestBedForwardDecompositionRadix2 (stage_a, kFFTSize, kNumBlocks, phase);
+    TestBedForwardDecompositionRadix2 (stage_a, kFFTSize, 1, kNumBlocks, phase);
 
     // Level 1 decompositions happen in the second half of phases
     if (phase >= kNumBlocks / 2)
     {
         auto relative_phase = phase - (kNumBlocks / 2);
-
-        TestBedForwardDecompositionRadix2 (stage_a, kFFTSize / 2, kNumBlocks / 2, relative_phase);
-        TestBedForwardDecompositionRadix2 (
-            stage_a + (kFFTSize / 2), kFFTSize / 2, kNumBlocks / 2, relative_phase);
+        TestBedForwardDecompositionRadix2 (stage_a, kFFTSize, 2, kNumBlocks / 2, relative_phase);
     }
 
     // Level 2 decompositions happen in the last quarter of phases
     if (phase >= 3 * (kNumBlocks / 4))
     {
         auto relative_phase = phase % (kNumBlocks / 4);
-
-        TestBedForwardDecompositionRadix2 (stage_a, kFFTSize / 4, kNumBlocks / 4, relative_phase);
-        TestBedForwardDecompositionRadix2 (
-            stage_a + (kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, relative_phase);
-        TestBedForwardDecompositionRadix2 (
-            stage_a + (2 * kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, relative_phase);
-        TestBedForwardDecompositionRadix2 (
-            stage_a + (3 * kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, relative_phase);
+        TestBedForwardDecompositionRadix2 (stage_a, kFFTSize, 4, kNumBlocks / 4, relative_phase);
     }
 }
 
@@ -130,28 +136,16 @@ static void HandleStageC (int kBlockSize,
 {
     auto stage_c = stage_buffers.GetStage (StageBuffers::kC)->GetWritePointer (0);
 
-    // Level 2 decompositions happen in the last quarter of phases
+    // Level 2 decompositions happen in the first quarter of phases
     if (phase < (kNumBlocks / 4))
-    {
-        TestBedInverseDecompositionRadix2 (stage_c, kFFTSize / 4, kNumBlocks / 4, phase);
-        TestBedInverseDecompositionRadix2 (
-            stage_c + (kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, phase);
-        TestBedInverseDecompositionRadix2 (
-            stage_c + (2 * kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, phase);
-        TestBedInverseDecompositionRadix2 (
-            stage_c + (3 * kFFTSize / 4), kFFTSize / 4, kNumBlocks / 4, phase);
-    }
+        TestBedInverseDecompositionRadix2 (stage_c, kFFTSize, 4, kNumBlocks / 4, phase);
 
-    // Level 1 decompositions happen in the second half of phases
+    // Level 1 decompositions happen in the first half of phases
     if (phase < kNumBlocks / 2)
-    {
-        TestBedInverseDecompositionRadix2 (stage_c, kFFTSize / 2, kNumBlocks / 2, phase);
-        TestBedInverseDecompositionRadix2 (
-            stage_c + (kFFTSize / 2), kFFTSize / 2, kNumBlocks / 2, phase);
-    }
+        TestBedInverseDecompositionRadix2 (stage_c, kFFTSize, 2, kNumBlocks / 2, phase);
 
     // Level 0 decompositions happen in every phase
-    TestBedInverseDecompositionRadix2 (stage_c, kFFTSize, kNumBlocks, phase);
+    TestBedInverseDecompositionRadix2 (stage_c, kFFTSize, 1, kNumBlocks, phase);
 }
 
 TEST_CASE ("decomposing a 16b partition")
