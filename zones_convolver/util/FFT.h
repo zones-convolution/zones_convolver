fe6d623db03b02cd889457d8d88ac5675796f59f
LeonPS <leon@leonps.com>
2024-03-10 17:41:32 +0000

    More cleanup

--- zones_convolver/util/FFT.h
+++ zones_convolver/util/FFT.h
@@ -1,6 +1,4 @@
 #pragma once
 
-#include <juce_dsp/juce_dsp.h>
-
 void ForwardFFTUnordered (std::complex<float> * data, std::size_t num_points);
 void InverseFFTUnordered (std::complex<float> * data, std::size_t num_points);
