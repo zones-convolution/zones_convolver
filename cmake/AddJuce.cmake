5f8b5fa22a9f46854fc3d249bc3d755288174cfe
LeonPS <leon@leonps.com>
2024-11-14 14:14:16 +0000

    Bump JUCE to 8.0.3

--- cmake/AddJuce.cmake
+++ cmake/AddJuce.cmake
@@ -3,5 +3,5 @@ FetchContent_Declare(JUCE
         GIT_TAG origin/master
         GIT_SHALLOW TRUE
         GIT_PROGRESS TRUE
-        FIND_PACKAGE_ARGS 8.0.1)
+        FIND_PACKAGE_ARGS 8.0.3)
 FetchContent_MakeAvailable(JUCE)
